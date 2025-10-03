665feceecafde0b63ff807043373560a939ea298
BuildTools <unconfigured@null.spigotmc.org>
2025-10-03 21:49:28 +0900

    Add Tailwind setup and configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // ✅ Tailwind 적용 CSS 불러오기
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
