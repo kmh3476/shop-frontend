99ab68eb78aa60a8eb934de0686ee490511a63d8
BuildTools <unconfigured@null.spigotmc.org>
2025-10-09 16:47:27 +0900

    Add Tailwind setup and configuration

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -242,6 +242,7 @@ function MainLayout() {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
+        {/**/}
       >
         <motion.h2
           className="text-3xl font-bold text-black mb-4"
