5d99315a0643f1601822f3dd2963c77f1c57b6b9
BuildTools <unconfigured@null.spigotmc.org>
2025-10-13 09:54:01 +0900

    Add Tailwind setup and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -170,7 +170,7 @@ function Navigation() {
           right: 0,
           width: isOpen
             ? isMobile
-              ? "38dvw" // 모바일: 전체화면
+              ? "100dvw" // 모바일: 전체화면
               : "38vw" // 데스크탑: 3분의 2만 덮기
             : "0",
             minWidth: isMobile ? "100%" : "auto", // 안정성 추가
