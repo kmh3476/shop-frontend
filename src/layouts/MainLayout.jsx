c479ffcd1a632b50943b05d50e6e6200134916b2
BuildTools <unconfigured@null.spigotmc.org>
2025-10-31 12:23:43 +0900

    Add Tailwind setup and configuration

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -23,7 +23,7 @@ function MainLayout() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:1337";
+        const apiUrl = import.meta.env.VITE_API_URL || "https://shop-backend-1-dfsl.onrender.com";
         const response = await axios.get(`${apiUrl}/api/products`);
         if (Array.isArray(response.data)) {
           setAllProducts(response.data);
