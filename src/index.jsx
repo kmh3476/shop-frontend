28803f1043c2a5e5ec85cece2267fdd7e82807c8
BuildTools <unconfigured@null.spigotmc.org>
2025-11-11 13:07:16 +0900

    Add Tailwind setup and configuration

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import "./i18n"; // ✅ i18next 초기화
+import "./api/authapi"; // ✅ axios 다국어 헤더 초기화
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'   // ✅ Tailwind 적용 CSS 불러오기
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
