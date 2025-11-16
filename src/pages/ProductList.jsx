2afa8bd88724fe66a7d13a483f4d04417e45f35c
BuildTools <unconfigured@null.spigotmc.org>
2025-11-16 13:59:58 +0900

    Add Tailwind setup and configuration

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -335,9 +335,9 @@ function ProductList() {
       <section
   className="
     grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-    gap-x-2 gap-y-4
-    w-full max-w-[1900px] mx-auto
-    px-1 sm:px-2
+    gap-x-1 gap-y-3
+    w-full max-w-[2500px] mx-auto
+    px-1 sm:px-1
   "
 >
 
