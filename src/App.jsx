a8aba47ae942d12378da1edddfcb2685d44c6772
BuildTools <unconfigured@null.spigotmc.org>
2025-11-18 11:20:23 +0900

    Add Tailwind setup and configuration

--- src/App.jsx
+++ src/App.jsx
@@ -428,7 +428,7 @@ useEffect(() => {
   <Mail
     style={{
       position: "absolute",
-      top: "12x",   // ← 모바일은 더 내려야 닉네임 아래로 나옴
+      top: "12px",   // ← 모바일은 더 내려야 닉네임 아래로 나옴
       right: "240px",
       width: "40px",
       height: "40px",
